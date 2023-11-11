e54f7b91ab02550de7801a6dd4d9fb0f06ed3935
Mahjabeen Perampalli <149791813+MAhjabeen96@users.noreply.github.com>
2023-11-11 16:43:18 +0530

    Update 3.sql

--- SQL-Queries/3.sql
+++ SQL-Queries/3.sql
@@ -1,17 +1,14 @@
 /*3.Find the Retailer with the highest total sales in each region?*/
-WITH max_region_sales AS (
-    SELECT l.region, MAX(s.total_sales) as max_total_sales
-    FROM adidas_sales s
-    JOIN locations l ON s.transaction_id = l.transaction_id
-    GROUP BY l.region
-)
-SELECT l.region,s.retailer, SUM(s.total_sales) as total_sales
-FROM adidas_sales s
-JOIN locations l ON s.transaction_id = l.transaction_id
-JOIN max_region_sales mrs ON l.region = mrs.region
-WHERE s.total_sales = mrs.max_total_sales
-GROUP BY l.region, s.retailer
-ORDER BY total_sales desc;
+SELECT region, retailer, total_region_sales 
+ FROM ( SELECT l.region, s.retailer, SUM(s.total_sales) AS total_region_sales, 
+ ROW_NUMBER() OVER (PARTITION BY l.region ORDER BY SUM(s.total_sales) DESC) AS sales_rank 
+ FROM adidas_sales s 
+ JOIN locations l 
+ ON s.transaction_id = l.transaction_id 
+ GROUP BY l.region, s.retailer
+ order by total_region_sales desc 
+ ) AS rankedSales 
+ WHERE sales_rank = 1;
 
 
 
